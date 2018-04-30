0956ca2dd082e4dad39274bfdd96faeb43845023
Verachidera <38852860+Verachidera@users.noreply.github.com>
2018-04-30 08:55:37 +0000

    Add files via upload
    Python arguments with different functions

---
+++ vera fucntion python.py
@@ -0,0 +1,32 @@
+#function
+def add(x,y):
+    sum=x+y
+    print(sum)
+       
+       
+add(12,1)
+add(20,14)
+
+def subtr(x,y):
+    subtr=x-y
+    print(subtr)
+
+subtr(30,12)
+subtr(100,67)
+
+def div(x,y):
+    div=x/y
+    print(div)
+
+div(40,20)
+div(111,13)
+
+def mult(x,y):
+    mult=x*y
+    print(mult)
+
+mult(12,13)
+mult(20,17)
+
+
+
