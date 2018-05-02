ae0efc5044b741759be64aa0a1e4bcce53626893
Verachidera <38852860+Verachidera@users.noreply.github.com>
2018-05-03 00:23:20 +0100

    Add files via upload

---
+++ vera loop 2-5-18.py
@@ -0,0 +1,31 @@
+#declare var fizz & buzz
+count = 3
+while(count>23):
+    print("fizz")
+    count+=3
+count = 5
+while(count<26):
+    print("buzz")
+    count+=5
+count = 3
+while(count>45):
+    print("fizz")
+    count+=3
+count = 5
+while(count<100):
+    print("buzz")
+    count+=5
+count = fizz
+while(count>43):
+    print("fizz")
+    count+=3
+count = buzz
+while(count<90):
+    print("buzz")
+    count+=5
+count = fizzbuzz
+while(count<buzz>fizz):
+    print("fizzbuzz")
+    count+=fizzbuzz
+
+
